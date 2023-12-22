e7aba6f03b7412b900d05fcecb8ff778b990821b
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2023-12-21 19:06:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ import stanza
 
 from gensim.models import Word2Vec
 from gensim.models.phrases import Phraser
-from stanza.server import CoreNLPClient
-from stanza.server.ud_enhancer import UniversalEnhancer
+#from stanza.server import CoreNLPClient
+#from stanza.server.ud_enhancer import UniversalEnhancer
 
 import dictionary_funcs
 import project_config as cfg
