f856cd0a2fc18784b451bfde9161dc4b942907ac
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2023-12-22 10:26:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,34 +21,40 @@ from nltk.tokenize import sent_tokenize
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
 
+
 doc = st.text_input("Enter chief officer's response:")
 
-#client = CoreNLPClient(properties={"ner.applyFineGrained": "false", "annotators": "tokenize, ssplit, pos, lemma, ner, depparse",}, endpoint="http://64.71.255.204:9002", start_server=stanza.server.StartServer.DONT_START, timeout=120000000,be_quiet=True,)
-#client.start()
+a, bi_phrase = urlretrieve("https://www.dropbox.com/scl/fi/ke1dk8kquwau2igkylvjw/bi_phrase.mod?rlkey=stmh2h26bv5wkunqiw8nh0kww&dl=1", "bi_phrase.mod")
+b, tri_phrase = urlretrieve("https://www.dropbox.com/scl/fi/nvxsx2a9uaj474jh83wfz/tri_phrase.mod?rlkey=ogxenfkeuqy9lulumdktjvnrp&dl=1", "tri_phrase.mod")
 
-#bigram_model = Phraser.load("bi_phrase.mod")
-#trigram_model = Phraser.load("tri_phrase.mod")
-#w2v_model = Word2Vec.load("w2v.mod")
+bigram_model = Phraser.load("bi_phrase.mod")
+trigram_model = Phraser.load("tri_phrase.mod")
+w2v_model = Word2Vec.load("w2v.mod")
 # read document-freq
-#with open("df_dict.pkl", "rb") as f:
-    #df_dict = pickle.load(f)
-
+with open("df_dict.pkl", "rb") as f:
+    df_dict = pickle.load(f)
 
 def remove_NER(line):
     NERs = re.compile("(\[NER:\w+\])(\S+)")
     line = re.sub(NERs, r"\1", line)
     return line
 
+
 def remove_puct_num(line):
     tokens = line.strip().lower().split()
     tokens = [re.sub("\[pos:.*?\]", "", t) for t in tokens]
     # these are tagged bracket and parenthesises
     if cfg.options.REMOVE_STOPWORDS:
-        puncts_stops = (set(["-lrb-", "-rrb-", "-lsb-", "-rsb-", "'s"]) | cfg.options.STOPWORDS)
+        puncts_stops = (
+            set(["-lrb-", "-rrb-", "-lsb-", "-rsb-", "'s"]) | cfg.options.STOPWORDS
+        )
     else:
         puncts_stops = set(["-lrb-", "-rrb-", "-lsb-", "-rsb-", "'s"])
     # filter out numerics and 1-letter words as recommend by https://sraf.nd.edu/textual-analysis/resources/#StopWords
-    tokens = filter(lambda t: any(c.isalpha() for c in t) and t not in puncts_stops and len(t) > 1,tokens,)
+    tokens = filter(
+        lambda t: any(c.isalpha() for c in t) and t not in puncts_stops and len(t) > 1,
+        tokens,
+    )
     return " ".join(tokens)
 
 
@@ -58,80 +64,96 @@ def clean(doc):
     cleaned = [functools.reduce(lambda obj, func: func(obj), [remove_NER, remove_puct_num], line,) for line in lines]
     return "\n".join(cleaned)
 
-def sentence_mwe_finder(
-    sentence_ann, dep_types=set(["mwe", "compound", "compound:prt", "fixed"])):
-    WMEs = [x for x in sentence_ann.enhancedPlusPlusDependencies.edge if x.dep in dep_types]
-    wme_edges = []
-    for wme in WMEs:
-        edge = sorted([wme.target, wme.source])
-        # Note: (-1) because edges in WMEs use indicies that indicate the end of a token (tokenEndIndex)
-        # (+ sentence_ann.token[0].tokenBeginIndex) because
-        # the edges indices are for current sentence, whereas tokenBeginIndex are for the document.
-        wme_edges.append([end - 1 + sentence_ann.token[0].tokenBeginIndex for end in edge])
-    return wme_edges
 
-def sentence_NE_finder(sentence_ann):
-    NE_edges = []
-    NE_types = []
-    for m in sentence_ann.mentions:
-        edge = sorted([m.tokenStartInSentenceInclusive, m.tokenEndInSentenceExclusive])
-        # Note: edge in NEs's end index is at the end of the last token
-        NE_edges.append([edge[0], edge[1] - 1])
-        NE_types.append(m.entityType)
-    return NE_edges, NE_types
+def Annotate(doc):
+    doc = doc.lower()
+    Annotations = []
+    def Tokenizer(text):
+        tokens = nltk.word_tokenize(text)
+        tokenized = ( " ".join([w for w in tokens])) 
+        return tokenized
+        
+    tokenized = Tokenizer(doc)
+    sentences = nltk.sent_tokenize(tokenized)
+    Sents = []
+    Annotated = {'sentence' : "", 'token' : ""}
+    
+    for sentence in sentences:
+        Sents.append(sentence)
+        
+        def POS(text): 
+            tagged = nltk.pos_tag(text.split())
+            return tagged
+
+        tags = POS(sentence)
+
+        def WordNet(pos):
+            if pos.startswith('J'):
+                return wordnet.ADJ
+            elif pos.startswith('V'):
+                return wordnet.VERB
+            elif pos.startswith('N'):
+                return wordnet.NOUN
+            elif pos.startswith('R'):
+                return wordnet.ADV
+            else:         
+                return None
+
+        def GetWT(text):  
+            WordNets = []   
+            for t in text:
+                WordPos = (t[0], WordNet(t[1]), t[1])
+                WordNets.append(WordPos)
+            return WordNets
+    
+        Wordnet = GetWT(tags)
+        lemmatizer = WordNetLemmatizer()
+    
+        def POSLEM(text):
+            sent = ""
+            for w, t, tag in text:
+                if t is None:
+                    lemma = "".join(lemmatizer.lemmatize(w))
+                else:       
+                    lemma = "".join(lemmatizer.lemmatize(w,t))   
+                def tokens(lemma, tag):
+                    token = {'pos' : "", 'lemma' : ""}
+                    token['lemma'] = lemma
+                    token['pos'] = tag
+                    return token
+                token = tokens(lemma, tag)
+                Annotations.append(token)
+                sent = sent + " " + lemma
+            return sent, Annotations
+                
+        lemmatized, Annotations = POSLEM(Wordnet) 
+    Annotated['sentence'] = Sents
+    Annotated['token'] = Annotations
+        
+    return Annotated
 
 
-def edge_simplifier(edges):
-    edge_sources = set([])  # edge that connects next token
-    for e in edges:
-        if e[0] + 1 == e[1]:
-            edge_sources.add(e[0])
-        else:
-            for i in range(e[0], e[1]):
-                edge_sources.add(i)
-    return edge_sources
+def process_sentence(doc):
+    sentence_ann = Annotate(doc)
+    sentence_parsed = []
+    NE_j = 0
+    for i, t in enumerate(sentence_ann['token']):
+        token_lemma = "{}[pos:{}]".format(t['lemma'], t['pos'])
+        sentence_parsed.append(token_lemma)
+    return " ".join(sentence_parsed)
 
 def process_document(doc):
-    doc_ann = client.annotate(doc)
+    doc_ann = Annotate(doc)
     sentences_processed = []
-    for i, sentence in enumerate(doc_ann.sentence):
+    for i, sentence in enumerate(doc_ann['sentence']):
         sentences_processed.append(process_sentence(sentence))
     return "\n".join(sentences_processed)
-
-def process_sentence(sentence_ann):
-    mwe_edge_sources = edge_simplifier(sentence_mwe_finder(sentence_ann))
-    # NE_edges can span more than two words or self-pointing
-    NE_edges, NE_types = sentence_NE_finder(sentence_ann)
-    # For tagging NEs
-    NE_BeginIndices = [e[0] for e in NE_edges]
-    # Unpack NE_edges to two-word edges set([i,j],..)
-    NE_edge_sources = edge_simplifier(NE_edges)
-    # For concat MWEs, multi-words NEs are MWEs too
-    mwe_edge_sources |= NE_edge_sources
-    sentence_parsed = []
-
-    NE_j = 0
-    for i, t in enumerate(sentence_ann.token):
-        token_lemma = "{}[pos:{}]".format(t.lemma, t.pos)
-        # concate MWEs
-        if t.tokenBeginIndex not in mwe_edge_sources:
-            token_lemma = token_lemma + " "
-        else:
-            token_lemma = token_lemma + "_"
-        # Add NE tags
-        if t.tokenBeginIndex in NE_BeginIndices:
-            if t.ner != "O":
-                # Only add tag if the word itself is an entity.
-                # (If a Pronoun refers to an entity, mention will also tag it.)
-                token_lemma = "[NER:{}]".format(NE_types[NE_j]) + token_lemma
-                NE_j += 1
-        sentence_parsed.append(token_lemma)
-    return "".join(sentence_parsed)
 
 # Concatenates must-have phrases in the text.
 def concat_must_have_phrases(text):
     all_seeds = []  # Load your must-have phrases here
-    pattern = "|".join(map(re.escape, [phrase.replace("_", " ") for phrase in all_seeds]))
+    pattern = "|".join(
+        map(re.escape, [phrase.replace("_", " ") for phrase in all_seeds]))
     text = re.sub(pattern, lambda match: match.group().replace(" ", "_"), text)
     return text
 
@@ -177,10 +199,9 @@ vectorized_new_text = clean_and_vectorize(doc_processed)[1]
 Selected_tab = st.sidebar.selectbox("Select a tab", ["Marketing Concepts\' Dimensions", "Marketing Concepts"])
 
 st.write("##### Results")
-       
 
 def dims(aspect):
-    dict_path = f'Documents/Ivey/Python/ECT/{aspect}_expanded_dict.csv'
+    dict_path = f'{aspect}_expanded_dict.csv'
     expanded_dict = dictionary_funcs.read_dict_from_csv(dict_path)[0]
     expanded_dict_vectors = {}
     for dim in expanded_dict:
@@ -195,8 +216,6 @@ def dims(aspect):
         for dim in expanded_dict_vectors:
             aspect_scores[dim] = np.dot(vectorized_new_text, expanded_dict_vectors[dim])
     return aspect_scores
-
-
 
 def full(aspect):
     dict_path = f'{aspect}_expanded_dict.csv'
@@ -213,22 +232,28 @@ def full(aspect):
         aspect_scores = {}
         for dim in expanded_dict_vectors:
             aspect_scores[dim] = np.dot(vectorized_new_text, expanded_dict_vectors[dim])
-            
         full = sum(aspect_scores.values()) / len(aspect_scores) 
     return full
 
 a = 0
-
 Aspects = {'capabilities': a, 'excellence':a, 'orientation':a}
+
+Dimensions = {'capabilities': {'Marketing Ecosystem': a, 'End User': a, 'Marketing Agility': a}, 'excellence' : {'Marketing Information Managament': a, 
+              'Marketing Planning Capabilities': a, 'Marketing Implementation Capabilities': a, 'Pricing Capabilities': a, 
+               'Product Development Capabilities': a, 'Channel Management':a, 'Marketing Communication Capabilities': a}, 'orientation' : {'Selling Capabilities': a, 'Customer Orientation': a, 'Competitor Orientation':a,
+               'Interfunctional Coordination':a, 'Long-term Focus':a, 'Profit Focus':a, 'Intelligence Generation':a, 'Intelligence Dissemination' :a,
+                'Responsiveness': a}}
 
 for aspect in Aspects:
     Aspects[aspect] = full(aspect)
-    #for dim in Dimensions:
-        #Dimensions[dim] = dims(aspect)
-    
+
+for aspect in Dimensions:
+    Dimensions[aspect] = dims(aspect)
+
+
     
 if Selected_tab == "Marketing Concepts\' Dimensions":
     st.write("Marketing Capabilities Dimensions", Dimensions)
     
 elif Selected_tab == "Marketing Conceps":
-    st.write("Marketing Concepts", "none")
+    st.write("Marketing Concepts", Aspects)
