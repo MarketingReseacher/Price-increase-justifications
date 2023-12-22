e5733d8fcffb65cd1458a31b8802d94088fe4f55
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2023-12-22 16:11:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ else:
 
 def getfiles():
     conn = st.connection('gcs', type=FilesConnection)
-    word2vec = conn.read("ectcalculator/w2v.mod", input_format="mod", ttl=3600)
+    word2vec = conn.read("ectcalculator/w2v.mod", ttl=3600)
 
 getfiles()
 
