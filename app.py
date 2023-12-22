b8281238d9012671b0d2cb9bdb0308d1cafbb93a
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2023-12-22 12:14:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ doc = st.text_input("Enter chief officer's response:")
 a, bi_phrase = urlretrieve("https://www.dropbox.com/scl/fi/ke1dk8kquwau2igkylvjw/bi_phrase.mod?rlkey=stmh2h26bv5wkunqiw8nh0kww&dl=1", "bi_phrase.mod")
 #bigram_model = gensim.models.Phraser.load("bi_phrase.mod")
 
-a, w2vfile1 = urlretrieve("https://www.dropbox.com/scl/fi/tz8gd9s1wlp3af8oajbih/w2v.mod.wv.vectors.npy?rlkey=t6yx9cweowuz73gpnr8a7rq13&dl=1", "w2v.mod.wv.vectors.npy")
-a, w2vfile2 = urlretrieve("https://www.dropbox.com/scl/fi/3ygttjde6wj8m5glg9tfd/w2v.mod.syn1neg.npy?rlkey=y2utzsl7styx61ouvdz4ls7jj&dl=1", "w2v.mod.syn1neg.npy")
+a, w2vfile1 = urlretrieve("https://www.dropbox.com/scl/fi/tz8gd9s1wlp3af8oajbih/w2v.mod.wv.vectors.npy?rlkey=t6yx9cweowuz73gpnr8a7rq13&dl=1", "f.wv.vectors.npy")
+a, w2vfile2 = urlretrieve("https://www.dropbox.com/scl/fi/3ygttjde6wj8m5glg9tfd/w2v.mod.syn1neg.npy?rlkey=y2utzsl7styx61ouvdz4ls7jj&dl=1", "f.syn1neg.npy")
 
 
 with open("w2v.mod", "r") as f:
