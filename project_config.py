80dde6f382c6bc46e43060600d73f00b687a6596
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2023-12-21 19:50:58 -0500

    Update project_config.py

--- project_config.py
+++ project_config.py
@@ -2,25 +2,6 @@ from __future__ import annotations
 
 from pathlib import Path
 
-
-class DIR:
-    raw_data = Path(data, "raw_xml")
-    dfs = Path(data, "dfs")
-    text_files = Path(data, "text_files")
-    database = Path(project_dir, "database")
-    models = Path("models")
-    output = Path(project_dir, "output")
-    seeds = Path(project_dir, "seeds")
-    log_files = Path(output, "log_files")
-    processed_corpus_path = Path(text_files, "processed")
-    processed_out_dir = processed_corpus_path / "parsed"
-    cleaned_out_dir = processed_corpus_path / "unigram"
-    broken_phrase_out_dir = processed_corpus_path / "broken_phrases"
-    bigram_out_dir = processed_corpus_path / "bigram"
-    trigram_out_dir = processed_corpus_path / "trigram"
-    database.mkdir(exist_ok=True, parents=True)
-
-
 class coreNLP_cfg:
     RAM = "30g"
     THREADS = 12
