ee1b6363c61a3a4f9d047876df551841469c8215
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-07 12:58:26 -0400

    Create main_page.py

---
+++ main_page.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown("## Welcome")
+st.write("The paucity of data on governmental regulatory agencies’ investigation of safety defects in products restricts academics', managers', regulators', and public policymakers' knowledge about the determinants of a regulator’s decision to open an investigation, the process it follows  between opening and closing of an investigation, and the outcomes of the investigation when it is closed.")
+st.write("We provide two Microsoft Excel Worksheet data files, one capturing data for the investigations opened and the other for the investigations closed. The data include numeric and textual fields, allowing researchers to examine regulatory investigations of product defects")
+st.write("Visit the **Data Visualisation** page for creating interactive graphs using our data.")
+st.sidebar.markdown("# Main page")
