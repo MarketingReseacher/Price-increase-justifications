6c1470652af0cb574b89666d9fd66e23b7d7b0bf
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-08 10:01:05 -0400

    Update 01_Investigation_Variables.py

--- pages/01_Investigation_Variables.py
+++ pages/01_Investigation_Variables.py
@@ -21,13 +21,16 @@ def GetLabels(x):
         Label = "Investigation Type"
     return Label
 
-def labeling(val):
-    return f'{val / 100 * len(Data):.0f}\n{val:.0f}%'
 
-def PlotPie(Data, var):
+
+def PlotPie(df, var):
+  
+    def labeling(val):
+      return f'{val / 100 * len(df):.0f}\n{val:.0f}%'
+      
     label = GetLabels(var)
     fig, (ax1) = plt.subplots(ncols=1, figsize=(10, 5))
-    Data.groupby(var).size().plot(kind='pie', autopct=labeling, colors=["#C00000", '#FF9999', '#00CCCC', '#49D845', '#CCCC00', '#808080'], textprops={'fontsize': 8}, ax=ax1)
+    df.groupby(var).size().plot(kind='pie', autopct=labeling, colors=["#C00000", '#FF9999', '#00CCCC', '#49D845', '#CCCC00', '#808080'], textprops={'fontsize': 8}, ax=ax1)
     ax1.set_title(f'Pie Chart of {var}')
     return fig
 
