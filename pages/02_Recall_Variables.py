465c94614f59b142affd83a434d0c4ca574ee871
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-08 08:06:17 -0400

    Update 02_Recall_Variables.py

--- pages/02_Recall_Variables.py
+++ pages/02_Recall_Variables.py
@@ -13,35 +13,28 @@ def ReadData():
 
 Data = ReadData()
 
-def GetColors(x):  
-    if x == "FirstComplaintToInvOpening":
-        Color = "#8FAADC"
-    elif x == "InvOpeningToClosing":
-        Color = "#7030A0"
-    elif x == "InvClosingToRecall":
-        Color = "#C00000"
-    else:
-        Color = "#FFC000"
-    return Color
-
 def GetLabels(x):  
-    if x == "FirstComplaintToInvOpening":
-        Label = "First Complaint to Investigation Opening"
-    elif x == "InvOpeningToClosing":
-        Label = "Investigation Opening to Closing"
-    elif x == "InvClosingToRecall":
-        Label = "Investigation Closing to Recall"
+    if x == "RecallSize":
+        Label = "Recall Size"
+    elif x == "RecallScope":
+        Label = "Recall Scope"
+    elif x == "NoManufacturers":
+        Label = "No. of Distinct Manufacturers of Recalled Products"
+      elif x == "NoPDUptoQuarterOfRcl":
+        Label = "No. of Product Damage Reports Up to Quarter of Recall"
+    elif x == "NoDIUptoQuarterOfRcl":
+        Label = "No. of Deaths Up to Quarter of Recall"
+    elif x == "NoIIUptoQuarterOfRcl":
+        Label = "No. of Injuries Up to Quarter of Recall"
     else:
-        Label = "Recall to Owner Notification Date"
+        Label = "No. of Death and Injury Reports Up to Quarter of Recall"
     return Label
 
-def PlotHist(x, var):
 
+def PlotHist(x, var):
     fig, ax = plt.subplots()
     Label = GetLabels(var)
-    Color = GetColors(var)
-    
-    plt.hist(x,  color=Color)
+    plt.hist(x)
     plt.title(f'Histogram of {Label}', size=12)
     plt.xlabel(Label, size=10, style= "italic")
     plt.ylabel("Frequency", size=12)
@@ -55,41 +48,50 @@ def PlotBox(x, var):
     fig, ax = plt.subplots()
     x = x.dropna()
     Label = GetLabels(var)
-    Color = GetColors(var)
-    
     plt.boxplot(x,  patch_artist=True)
     plt.title(f'Boxplot of {Label}', size=12)
     plt.ylabel(Label, size=12, style= "italic")
     fig.set_figheight(6)
     fig.set_figwidth(8)
-
     return fig
     
 
 
 
-Selected_var = st.sidebar.selectbox("Select a process variable", ["First Complaint to Investigation Opening", "Investigation Opening to Closing", "Investigation Closing to Recall", "Recall to Owner Notification Date"])
+Selected_var = st.sidebar.selectbox("Select a recall variable", ["Recall Size", "Recall Scope", "No. of Distinct Manufacturers of Recalled Products", "No. of Product Damage Reports Up to Quarter of Recall", "No. of Deaths Up to Quarter of Recall", "No. of Injuries Up to Quarter of Recall", "No. of Death and Injury Reports Up to Quarter of Recall"])
 Selected_graph = st.sidebar.selectbox("Select a graph", ["Histogram", "Boxplot"])
 
 if Selected_graph == "Histogram":
-  if Selected_var == "First Complaint to Investigation Opening":
-    plt = PlotHist(Data['FirstComplaintToInvOpening'], 'FirstComplaintToInvOpening')
-  elif Selected_var == "Investigation Opening to Closing":
-    plt = PlotHist(Data['InvOpeningToClosing'], 'InvOpeningToClosing')
-  elif Selected_var == "Investigation Closing to Recall":
-    plt = PlotHist(Data['InvClosingToRecall'], 'InvClosingToRecall')
+  if Selected_var == "Recall Size":
+    plt = PlotHist(Data['RecallSize'], 'RecallSize')
+  elif Selected_var == "Recall Scope":
+    plt = PlotHist(Data['RecallScope'], 'RecallScope')
+  elif Selected_var == "No. of Distinct Manufacturers of Recalled Products":
+    plt = PlotHist(Data['NoManufacturers'], 'NoManufacturers')
+  elif Selected_var == "No. of Product Damage Reports Up to Quarter of Recall":
+    plt = PlotHist(Data['NoPDUptoQuarterOfRcl'], 'NoPDUptoQuarterOfRcl')
+  elif Selected_var == "No. of Deaths Up to Quarter of Recall":
+    plt = PlotHist(Data['NoDIUptoQuarterOfRcl'], 'NoDIUptoQuarterOfRcl')
+  elif Selected_var == "No. of Injuries Up to Quarter of Recall":
+    plt = PlotHist(Data['NoIIUptoQuarterOfRcl'], 'NoIIUptoQuarterOfRcl')
   else:
-    plt = PlotHist(Data['RecallToOwnerNotification'], 'RecallToOwnerNotification')
+    plt = PlotHist(Data['NoIDUptoQuarterOfRcl'], 'NoIDUptoQuarterOfRcl')
   st.pyplot(plt)  
 
 else:
-  if Selected_var == "First Complaint to Investigation Opening":
-    plt = PlotBox(Data['FirstComplaintToInvOpening'], 'FirstComplaintToInvOpening')
-  elif Selected_var == "Investigation Opening to Closing":
-    plt = PlotBox(Data['InvOpeningToClosing'], 'InvOpeningToClosing')
-  elif Selected_var == "Investigation Closing to Recall":
-    plt = PlotBox(Data['InvClosingToRecall'], 'InvClosingToRecall')
+  if Selected_var == "Recall Size":
+    plt = PlotBox(Data['RecallSize'], 'RecallSize')
+  elif Selected_var == "Recall Scope":
+    plt = PlotBox(Data['RecallScope'], 'RecallScope')
+  elif Selected_var == "No. of Distinct Manufacturers of Recalled Products":
+    plt = PlotBox(Data['NoManufacturers'], 'NoManufacturers')
+  elif Selected_var == "No. of Product Damage Reports Up to Quarter of Recall":
+    plt = PlotBox(Data['NoPDUptoQuarterOfRcl'], 'NoPDUptoQuarterOfRcl')
+  elif Selected_var == "No. of Deaths Up to Quarter of Recall":
+    plt = PlotBox(Data['NoDIUptoQuarterOfRcl'], 'NoDIUptoQuarterOfRcl')
+  elif Selected_var == "No. of Injuries Up to Quarter of Recall":
+    plt = PlotBox(Data['NoIIUptoQuarterOfRcl'], 'NoIIUptoQuarterOfRcl')
   else:
-    plt = PlotBox(Data['RecallToOwnerNotification'], 'RecallToOwnerNotification')
+    plt = PlotBox(Data['NoIDUptoQuarterOfRcl'], 'NoIDUptoQuarterOfRcl')
   st.pyplot(plt)  
 
