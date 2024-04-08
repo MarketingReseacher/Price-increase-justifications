f681fea29da9ff26776fa63c6a52c2e14fbeef51
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-08 19:58:52 -0400

    Update 01_Investigation_Variables.py

--- pages/01_Investigation_Variables.py
+++ pages/01_Investigation_Variables.py
@@ -35,6 +35,16 @@ def PlotHist(x, var):
     fig.set_figwidth(8)
     return fig
 
+def PlotBox(x, var):
+    fig, ax = plt.subplots()
+    x = x.dropna()
+    plt.boxplot(x,  patch_artist=True)
+    plt.title(f'Boxplot of {var}', size=12)
+    plt.ylabel(var, size=12, style= "italic")
+    fig.set_figheight(6)
+    fig.set_figwidth(8)
+    return fig
+
 Selected_var = st.sidebar.selectbox("Select a variable", ["Investigation Type", "Population", "No. Complaints Reported to NHTSA", "No. Crashes and Fires Reported to NHTSA", "No. Injury Incidents Reported to NHTSA", "No. Injuries Reported to NHTSA", "No Fatality Incidents Reported to NHTSA", "No. Fatalities Reported to NHTSA", "No. Other Types Failures Reported to NHTSA", "No. Complaints Reported to the Manufacturer", "No. Crashes and Fires Reported to Manufacturer", "No. Injury Incidents Reported to Manufacturer",  "No. Injuries Reported to the Manufacturer", "No Fatality Incidents Reported to the Manufacturer", "No. Fatalities Reported to the Manufacturer", "No. Other Types Failures Reported to the Manufacturer", "No. Complaints Reported", "No. Crashes and Fires Reported", "No. Injury Incidents Reported",  "No. Injuries Reported", "No Fatality Incidents Reported", "No. Fatalities Reported", "No. Other Types Failures Reported", "Problem Definition Sentiment", "Summary Sentiment", "No. Product Damage Reports Up to Quarter Investigation", "No. Deaths Up to Quarter Investigation", "No. Injuries Up to Quarter Investigation", "No. Injury and Death Reports Up to Quarter Investigation"])
 Selected_Data = st.sidebar.selectbox("Select data", ["Opened Investigations", "Closed Investigations", "Opened and Closed Investigations"])
 Selected_graph = st.sidebar.selectbox("Select a graph", ["Pie", "Histogram", "Boxplot"])
