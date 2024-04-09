bd0a61589e608327062f3229fa569f79f7badfdd
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-09 16:33:55 -0400

    Update 01_Investigation_Variables.py

--- pages/01_Investigation_Variables.py
+++ pages/01_Investigation_Variables.py
@@ -71,7 +71,7 @@ if Selected_var != "Investigation Type":
       if label == Selected_var:
          columns=['Mean', 'Median', 'Standard Deviation', 'Min', 'Max']
          Sum = pd.DataFrame([[round(Data.loc[:, variable].mean(), 2), round(Data.loc[:, variable].median(), 2), round(Data.loc[:, variable].std(), 2), round(Data.loc[:, variable].min(), 2), round(Data.loc[:, variable].max(), 2)]], columns=columns)
-         table = Sum.to_html(index=False).replace('<td>', '<td align="center">')
+         table = Sum.to_html(index=False, justify="center")
          st.markdown(table, unsafe_allow_html=True)
          if Selected_graph == "Histogram":
            plt = PlotHist(MyDF[variable], Labels[variable])
