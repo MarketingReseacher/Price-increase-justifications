cd407ef24b7748cc2a7cd3bf8e7d66ac14666d2c
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-08 20:58:45 -0400

    Update 03_Recall_Variables.py

--- pages/03_Recall_Variables.py
+++ pages/03_Recall_Variables.py
@@ -32,14 +32,32 @@ def PlotBox(x, var):
     fig.set_figheight(6)
     fig.set_figwidth(8)
     return fig
+
+def PlotPie(df, var):
+    def labeling(val):
+      return f'{val / 100 * len(df):.0f}\n{val:.0f}%'
+    fig, (ax1) = plt.subplots(ncols=1, figsize=(10, 5))
+    df.groupby(var).size().plot(kind='pie', autopct=labeling, colors=["#C00000", '#FF9999', '#00CCCC', '#49D845', '#CCCC00', '#808080'], textprops={'fontsize': 8}, ax=ax1)
+    label = Labels[var]
+    ax1.set_title(f'Pie Chart of {label}')
+    return fig
     
 
-Labels = {'RecallSize': "Recall Size", 'RecallScope': "Recall Scope", 'NoNHTSACampaignNumbers': "No. NHTSA Campaign Numbers", 'NoManufacturers': "No. Distinct Manufacturers of Recalled Products", 'NoPDUptoQuarterOfRcl': "No. Product Damage Reports Up to Quarter of Recall", 'NoDIUptoQuarterOfRcl': "No. Deaths Up to Quarter of Recall", 'NoIIUptoQuarterOfRcl': "No. of Injuries Up to Quarter of Recall", 'NoIDUptoQuarterOfRcl': "No. Death and Injury Reports Up to Quarter of Recall"}
+Labels = {'RecallType': 'Recall Type', 'InfluencedBy': 'Influenced By', 'RecallSize': "Recall Size", 'RecallScope': "Recall Scope", 'NoNHTSACampaignNumbers': "No. NHTSA Campaign Numbers", 'NoManufacturers': "No. Distinct Manufacturers of Recalled Products", 'NoPDUptoQuarterOfRcl': "No. Product Damage Reports Up to Quarter of Recall", 'NoDIUptoQuarterOfRcl': "No. Deaths Up to Quarter of Recall", 'NoIIUptoQuarterOfRcl': "No. of Injuries Up to Quarter of Recall", 'NoIDUptoQuarterOfRcl': "No. Death and Injury Reports Up to Quarter of Recall"}
 
-Selected_var = st.sidebar.selectbox("Select a recall variable", ["Recall Size", "Recall Scope", "No. NHTSA Campaign Numbers", "No. Distinct Manufacturers of Recalled Products", "No. Product Damage Reports Up to Quarter of Recall", "No. Deaths Up to Quarter of Recall", "No. Injuries Up to Quarter of Recall", "No. Death and Injury Reports Up to Quarter of Recall"], help = "Select the variable you want to see a visual representation of")
-Selected_graph = st.sidebar.selectbox("Select a graph", ["Histogram", "Boxplot"], help = "Select Histogram or Boxplot for numerical variables, and Pie Chart for categorical variables.")
+Selected_var = st.sidebar.selectbox("Select a recall variable", ['Recall Type', 'Influenced By', "Recall Size", "Recall Scope", "No. NHTSA Campaign Numbers", "No. Distinct Manufacturers of Recalled Products", "No. Product Damage Reports Up to Quarter of Recall", "No. Deaths Up to Quarter of Recall", "No. Injuries Up to Quarter of Recall", "No. Death and Injury Reports Up to Quarter of Recall"], help = "Select the variable you want to see a visual representation of")
+Selected_graph = st.sidebar.selectbox("Select a graph", ["Pie Chart", "Histogram", "Boxplot"], help = "Select Histogram or Boxplot for numerical variables, and Pie Chart for categorical variables.")
 
-if Selected_graph == "Histogram":
+if Selected_graph == "Pie Chart":
+  if Selected_var == "Recall Type" or "Influenced By":
+    for variable, label in Labels.items():
+      if label == Selected_var:
+        plt = PlotPie(MyDF, Labels[variable])
+    st.pyplot(plt) 
+  else:
+    st.write("For numerical variables, please choose histogram or boxplot as graph type.") 
+
+elif Selected_graph == "Histogram":
   for variable, label in Labels.items():
     if label == Selected_var:
       plt = PlotHist(Data[variable], Labels[variable])
