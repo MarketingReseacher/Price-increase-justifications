57ea6980ae1112a71cc0f36bedb24a72465c7c69
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-08 21:45:55 -0400

    Update Main_page.py

--- Main_page.py
+++ Main_page.py
@@ -5,8 +5,8 @@ st.write("The paucity of data on governmental regulatory agencies’ investigati
 st.write("To remove this barrier, we provide two Microsoft Excel Worksheet data files, one capturing data for the investigations opened and the other for the investigations closed. The data include numeric and textual fields, allowing researchers to examine regulatory investigations of product defects. Specifically, the data set covers all investigations that the National Highway Traffic Safety Administration (NHTSA)—the U.S. regulator for automobile safety—opened and closed against 194 manufacturers between 2009 and 2021.")
 st.write("This website is an easy access tool designed by our research team, meant to enable researchers to examine the characteristics of our data easily and interactively.")
 st.write("You can visit the **Investigations Variables** page to obtain graphical representations of the variables related to the opening or closing of investigations.")
-st.write("Visit the **Process Variables** page to obtain graphical representations of our process-time variables.")
-st.write("Visit the **Recall Variables** page to obtain graphical representations of the variables pertaining to the outcomes of investigations (i.e., product recalls).")
+st.write("Visit the **Process Variables** page for graphs of our process-time variables.")
+st.write("Visit the **Recall Variables** page to obtain graphs of the variables pertaining to the outcomes of investigations (i.e., product recalls).")
 st.write("Lastly, visit the **Data Dictionary** page to preview and download a table that outlines the data type/format, computation method and formula, source, and examples for all the variables included in our data sets.")
 
 st.sidebar.markdown("# Main page")
