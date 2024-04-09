3790da21bbcbb5f2a407cf134f1d12263304b1a1
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-08 20:16:55 -0400

    Update Main_page.py

--- Main_page.py
+++ Main_page.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
 st.markdown("## Welcome")
-st.write("The paucity of data on governmental regulatory agencies’ investigation of safety defects in products restricts academics', managers', regulators', and public policymakers' knowledge about the determinants of a regulator’s decision to open an investigation, the process it follows  between opening and closing of an investigation, and the outcomes of the investigation when it is closed.")
-st.write("We provide two Microsoft Excel Worksheet data files, one capturing data for the investigations opened and the other for the investigations closed. The data include numeric and textual fields, allowing researchers to examine regulatory investigations of product defects")
-st.write("Visit the **Data Visualisation** page for creating interactive graphs using our data.")
+st.write("The paucity of data on governmental regulatory agencies’ investigation of product safety defects has restricted academics', managers', and public policymakers' knowledge of the determinants of a regulator’s decision to open an investigation, the process followed between opening and closing of an investigation, and the outcomes of the investigation when it is closed.")
+st.write("To remove this barrier, we provide two Microsoft Excel Worksheet data files, one capturing data for the investigations opened and the other for the investigations closed. The data include numeric and textual fields, allowing researchers to examine regulatory investigations of product defects. Specifically, the data set covers all investigations that the National Highway Traffic Safety Administration (NHTSA)—the U.S. regulator for automobile safety—opened and closed against 194 manufacturers between 2009 and 2021.")
+st.write("This website is an easy access tool designed by our research team, meant to enable researchers to examine the characteristics of our data easily and interactively.")
+st.write("You can visit the **Investigations Variables** page to obtain graphical representations of the variables leading to the opening or closing of investigations.")
+st.write("Visit the **Process Variables** page to obtain graphical representations of our process-time variables.")
+st.write("Visit the **Recall Variables** page to obtain graphical representations of the variables pertaining to the outcomes of investigations (i.e., product recalls).")
+st.write("Lastly, visit the **Data Dictionary** page to preview and download a table that outlines the data type/format, computation method and formula, source, and examples for all the variables included in our data sets.")
+
 st.sidebar.markdown("# Main page")
