8b7715963cc49a7393e34d4e69d2b6fef6e70aa0
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-08 20:30:56 -0400

    Update 01_Investigation_Variables.py

--- pages/01_Investigation_Variables.py
+++ pages/01_Investigation_Variables.py
@@ -50,6 +50,7 @@ Selected_var = st.sidebar.selectbox("Select a variable", ["Investigation Type", 
 Selected_Data = st.sidebar.selectbox("Select data", ["Opened Investigations", "Closed Investigations", "Opened and Closed Investigations"], help = "Select the data source.")
 Selected_graph = st.sidebar.selectbox("Select a graph", ["Pie Chart", "Histogram", "Boxplot"], help = "Select Histogram or Boxplot for numerical variables, and Pie Chart for categorical variables.")
 
+Numerics = ["Investigation Type", "Population", "No. Complaints Reported to NHTSA", "No. Crashes and Fires Reported to NHTSA", "No. Injury Incidents Reported to NHTSA", "No. Injuries Reported to NHTSA", "No Fatality Incidents Reported to NHTSA", "No. Fatalities Reported to NHTSA", "No. Other Types Failures Reported to NHTSA", "No. Complaints Reported to the Manufacturer", "No. Crashes and Fires Reported to Manufacturer", "No. Injury Incidents Reported to Manufacturer",  "No. Injuries Reported to the Manufacturer", "No Fatality Incidents Reported to the Manufacturer", "No. Fatalities Reported to the Manufacturer", "No. Other Types Failures Reported to the Manufacturer", "No. Complaints Reported", "No. Crashes and Fires Reported", "No. Injury Incidents Reported",  "No. Injuries Reported", "No Fatality Incidents Reported", "No. Fatalities Reported", "No. Other Types Failures Reported", "Problem Definition Sentiment", "Summary Sentiment", "No. Product Damage Reports Up to Quarter Investigation", "No. Deaths Up to Quarter Investigation", "No. Injuries Up to Quarter Investigation", "No. Injury and Death Reports Up to Quarter Investigation"]
 Labels = {"InvestigationType": "Investigation Type", "NoComplaintsReportedNHTSA": "No. Complaints Reported to NHTSA", "NoCrashesFiresReportedNHTSA": "No. Crashes and Fires Reported to NHTSA", "NoInjuryIncidentsReportedNHTSA": "No. Injury Incidents Reported to NHTSA", "NoInjuriesReportedNHTSA": "No. Injuries Reported to NHTSA", "NoFatalityIncidentsReportedNHTSA": "No Fatality Incidents Reported to NHTSA", "NoFatalitiesReportedNHTSA": "No. Fatalities Reported to NHTSA", "NoOtherFailuresReportedNHTSA": "No. Other Types Failures Reported to NHTSA", "NoComplaintsReportedMfr": "No. Complaints Reported to the Manufacturer", "NoCrashesFiresReportedMfr": "No. Crashes and Fires Reported to the Manufacturer", "NoInjuryIncidentsReportedMfr": "No. Injury Incidents Reported to the Manufacturer", "NoInjuriesReportedMfr": "No. Injuries Reported to the Manufacturer", "NoFatalityIncidentsReportedMfr": "No Fatality Incidents Reported to the Manufacturer", "NoFatalitiesReportedMfr": "No. Fatalities Reported to the Manufacturer", "NoOtherFailuresReportedMfr": "No. Other Types Failures Reported to the Manufacturer", "NoComplaintsReported": "No. Complaints Reported", "NoCrashesFiresReported": "No. Crashes and Fires Reported", "NoInjuryIncidentsReported": "No. Injury Incidents Reported", "NoInjuriesReported": "No. Injuries Reported", "NoFatalityIncidentsReported": "No Fatality Incidents Reported", "NoFatalitiesReported": "No. Fatalities Reported", "NoOtherFailuresReported": "No. Other Types Failures Reported", "PDSentiment": "Problem Definition Sentiment", "SummarySentiment": "Summary Sentiment", "NoPDUptoQuarter": "No. Product Damage Reports Up to Quarter Investigation", "NoDIUptoQuarter": "No. Deaths Up to Quarter Investigation", "NoIIUptoQuarter": "No. Injuries Up to Quarter Investigation", "NoIDUptoQuarter": "No. Injury and Death Reports Up to Quarter Investigation" }
 
 
@@ -70,62 +71,11 @@ if Selected_graph == "Pie Chart":
 if Selected_graph == "Histogram":
     if Selected_var == "Investigation Type":
         st.write("For a histogram or boxplot, please choose a numerical variable.")     
-    elif Selected_var == "No. Complaints Reported to NHTSA":
-        plt = PlotHist(MyDF["NoComplaintsReportedNHTSA"], Labels["NoComplaintsReportedNHTSA"])
-    elif Selected_var == "No. Crashes and Fires Reported to NHTSA":
-        plt = PlotHist(MyDF["NoCrashesFiresReportedNHTSA"], Labels["NoCrashesFiresReportedNHTSA"])
-    elif Selected_var == "No. Injury Incidents Reported to NHTSA":
-        plt = PlotHist(MyDF["NoInjuryIncidentsReportedNHTSA"], Labels["NoInjuryIncidentsReportedNHTSA"])   
-    elif Selected_var == "No. Injuries Reported to NHTSA": 
-        plt = PlotHist(MyDF["NoInjuriesReportedNHTSA"], Labels["NoInjuriesReportedNHTSA"]) 
-    elif Selected_var == "No Fatality Incidents Reported to NHTSA":
-        plt = PlotHist(MyDF["NoFatalityIncidentsReportedNHTSA"], Labels["NoFatalityIncidentsReportedNHTSA"])
-    elif Selected_var == "No. Fatalities Reported to NHTSA":
-        plt = PlotHist(MyDF["NoFatalitiesReportedNHTSA"], Labels["NoFatalitiesReportedNHTSA"])
-    elif Selected_var == "No. Other Types Failures Reported to NHTSA":
-        plt = PlotHist(MyDF["NoOtherFailuresReportedNHTSA"], Labels["NoOtherFailuresReportedNHTSA"]) 
-    elif Selected_var == "No. Complaints Reported to the Manufacturer" :
-        plt = PlotHist(MyDF["NoComplaintsReportedMfr"], Labels[ "NoComplaintsReportedMfr"])
-    elif Selected_var == "No. Crashes and Fires Reported to the Manufacturer":
-        plt = PlotHist(MyDF["NoCrashesFiresReportedMfr"], Labels["NoCrashesFiresReportedMfr"])
-    elif Selected_var == "No. Injury Incidents Reported to the Manufacturer":
-        plt = PlotHist(MyDF["NoInjuryIncidentsReportedMfr"], Labels["NoInjuryIncidentsReportedMfr"])
-    elif Selected_var == "No. Injuries Reported to the Manufacturer":
-        plt = PlotHist(MyDF["NoInjuriesReportedMfr"], Labels["NoInjuriesReportedMfr"])
-    elif Selected_var == "No Fatality Incidents Reported to the Manufacturer":
-        plt = PlotHist(MyDF["NoFatalityIncidentsReportedMfr"], Labels["NoFatalityIncidentsReportedMfr"])
-    elif Selected_var ==  "No. Fatalities Reported to the Manufacturer":
-        plt = PlotHist(MyDF["NoFatalitiesReportedMfr"], Labels["NoFatalitiesReportedMfr"])
-    elif Selected_var == "No. Other Types Failures Reported to the Manufacturer":
-        plt = PlotHist(MyDF["NoOtherFailuresReportedMfr"], Labels["NoOtherFailuresReportedMfr"])
-    elif Selected_var == "No. Complaints Reported":
-        plt = PlotHist(MyDF["NoComplaintsReported"], Labels["NoComplaintsReported"])
-    elif Selected_var == "No. Crashes and Fires Reported":
-        plt = PlotHist(MyDF["NoCrashesFiresReported"], Labels["NoCrashesFiresReported"])
-    elif Selected_var == "No. Injury Incidents Reported":
-        plt = PlotHist(MyDF["NoInjuryIncidentsReported"], Labels["NoInjuryIncidentsReported"])
-    elif Selected_var == "No. Injuries Reported":
-        plt = PlotHist(MyDF["NoInjuriesReported"], Labels["NoInjuriesReported"])
-    elif Selected_var == "No Fatality Incidents Reported":
-        plt = PlotHist(MyDF["NoFatalityIncidentsReported"], Labels["NoFatalityIncidentsReported"])
-    elif Selected_var == "No. Fatalities Reported":
-        plt = PlotHist(MyDF["NoFatalitiesReported"], Labels["NoFatalitiesReported"])
-    elif Selected_var == "No. Other Types Failures Reported":
-        plt = PlotHist(MyDF["NoOtherFailuresReported"], Labels["NoOtherFailuresReported"])
-    elif Selected_var == "Population":
-        plt = PlotHist(MyDF["Population"], Labels["Population"])
-    elif Selected_var == "Problem Definition Sentiment":
-        plt = PlotHist(MyDF["PDSentiment"], Labels["PDSentiment"])
-    elif Selected_var == "Summary Sentiment":
-        plt = PlotHist(MyDF["SummarySentiment"], Labels["SummarySentiment"])
-    elif Selected_var == "No. Product Damage Reports Up to Quarter Investigation":
-        plt = PlotHist(MyDF["NoPDUptoQuarter"], Labels["NoPDUptoQuarter"])
-    elif Selected_var == "No. Deaths Up to Quarter Investigation":
-        plt = PlotHist(MyDF["NoDIUptoQuarter"], Labels["NoDIUptoQuarter"])
-    elif Selected_var == "No. Injuries Up to Quarter Investigation":
-        plt = PlotHist(MyDF["NoIIUptoQuarter"], Labels["NoIIUptoQuarter"])
-    elif Selected_var == "No. Injury and Death Reports Up to Quarter Investigation":
-        plt = PlotHist(MyDF["NoIDUptoQuarter"], Labels["NoIDUptoQuarter"])
+    elif Selected_var in Numerics:
+      for variable, label in Labels.items():
+        if label == Selected_var:
+        plt = PlotHist(MyDF[variable], Labels[variable])
+
     st.pyplot(plt)
 
 
