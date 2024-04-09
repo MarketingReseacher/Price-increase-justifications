e9e1c57fb380a16295d065dd958faca377921eeb
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-08 21:42:30 -0400

    Update 03_Recall_Variables.py

--- pages/03_Recall_Variables.py
+++ pages/03_Recall_Variables.py
@@ -31,6 +31,9 @@ def PlotBox(x, var):
     plt.ylabel(var, size=12, style= "italic")
     fig.set_figheight(6)
     fig.set_figwidth(8)
+    quantiles = np.quantile(x, np.array([0.00, 0.25, 0.50, 0.75, 1.00]))
+    ax.set_yticks(quantiles)
+    ax.tick_params(axis='y', labelsize=6)
     return fig
 
 def PlotPie(df, var):
