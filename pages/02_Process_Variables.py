1a40337ac759322338d2168ccc1cfc36474cb83d
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-08 21:41:31 -0400

    Update 02_Process_Variables.py

--- pages/02_Process_Variables.py
+++ pages/02_Process_Variables.py
@@ -49,7 +49,8 @@ def PlotBox(x, var):
     fig.set_figheight(6)
     fig.set_figwidth(8)
     quantiles = np.quantile(x, np.array([0.00, 0.25, 0.50, 0.75, 1.00]))
-    ax.set_yticks(quantiles, fontsize=8)
+    ax.set_yticks(quantiles)
+    ax.tick_params(axis='y', labelsize=8)
     return fig
     
 
