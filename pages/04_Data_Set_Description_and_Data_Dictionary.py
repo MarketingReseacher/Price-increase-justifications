5449dea2f6ed337a17622895b4a03a7504db1f3a
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2024-04-11 07:37:31 -0400

    Update 04_Data_Set_Description_and_Data_Dictionary.py

--- pages/04_Data_Set_Description_and_Data_Dictionary.py
+++ pages/04_Data_Set_Description_and_Data_Dictionary.py
@@ -10,9 +10,10 @@ def ReadData():
   Data = pd.read_csv('Dictionary.csv')
   return Data
 
-st.image("Picture3.jpg", caption="Our data retrieval and data set construction procedure")
+st.markdown("#### Our data retrieval and data set construction procedure")
+st.image("Picture3.jpg")
 
 Data = ReadData()
-st.write(Data.head(10))
+st.write(Data.head(5))
 Dic = Data.to_csv(encoding='utf-8', index=False)
 st.download_button('Download dictionary', Dic, file_name='Dictionary.csv')
