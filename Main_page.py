e82f3bbce1f84a6f9b3cf2f65e66477912df0a0e
Marketing researcher <42955679+MarketingReseacher@users.noreply.github.com>
2025-06-16 07:14:28 -0400

    Update Main_page.py

--- Main_page.py
+++ Main_page.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.markdown("## Welcome")
 st.write("The purpose of this website is to provide easy access to a corpus of 300+ actual price increase letters collected by our research team.")
-st.write("We conducted a thorough search of Google, Reddit, and Factiva, to find real price increase letters shared by firms. We used terms such as “price increase,” “price hike,” “raising price,” “rising price,” and “new price,” as keywords in our search.")
-st.write("Our primary goal in doing this was to understand how firms justify their price increases in practice, and improve the ecological validity of our research.")
+st.write("We conducted a thorough search of Google, Reddit, and Factiva, to find examples of price increase letters or announcements shared by firms. We used terms such as “price increase,” “price hike,” “raising price,” “rising price,” and “new price,” as keywords in our search.")
+st.write("Our primary goal in conducting this search this was to understand how firms justify their price increases in practice, and to improve the ecological validity of our research.")
 st.write("This website is open-access and seeks to enable researchers and managers to examine our collected corpus of price increase letters easily and interactively.")
-st.write("Visit the **Browsing Letters** page to browse through the collected price increase letters by firm, date, or justification type.")
-st.write("You can also visit the **Descriptive Analysis** page to see relevant graphical representations.")
+st.write("Visit the **Browsing Letters** page to browse through the collected price increase letters by justification type, year, sector, or business model.")
+st.write("You can also visit the **Descriptive Analysis** page to see relevant statistical tests and graphical representations.")
 st.sidebar.markdown("# Main page")
 
